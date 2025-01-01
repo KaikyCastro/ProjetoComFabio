66fe10025539e450971e7bea85aeed53a64d04b9
Kaiky Castro <kaiky.castro7@hotmail.com>
2025-01-01 18:40:45 -0300

    Por hoje é só (eu acho)

--- ProjetoComFabio/main.py
+++ ProjetoComFabio/main.py
@@ -1,7 +1,7 @@
 from Usuario import Usuario
 from hashlib import sha256
-
-
+import customtkinter as ck
+ck
 usuario = Usuario("Fabio", 20, "134.301.994-09", "01/01/2000", "1234567", "Centro", "Sao Paulo", "SP", "11999999999")
 
 result = usuario.validarCPF(usuario.getCPF())
