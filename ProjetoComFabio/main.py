87a3e2d564def8888357d99528a66b7ca5dd528b
Kaiky Castro <kaiky.castro7@hotmail.com>
2025-01-05 19:19:36 -0300

    Banco de dados funcionando

--- ProjetoComFabio/main.py
+++ ProjetoComFabio/main.py
@@ -1,14 +1,17 @@
 from Tela import Login
-
+import mysql.connector 
 
 # Conexão com o banco de dados
-conexao = connector.connect(
+conexao = mysql.connector.connect(
     host='localhost',
     user='root',
     password='',
     database='cadastro')
 
 cursor = conexao.cursor()
+cursor.execute('INSERT INTO pessoas (nomev, nascimento, sexo, altura, peso, nacionalidade) VALUES ("Fabio", "2004-01-20", "F", "1.50", "130.0", "Africano")');
+conexao.commit()
+
 
 cursor.close()
 conexao.close()
