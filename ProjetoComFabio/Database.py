c9cd39b14940f0d2917d86916e23c47f19a60d41
Kaiky Castro <kaiky.castro7@hotmail.com>
2025-01-06 12:24:01 -0300

    Funções básicas do SQL funcionando

--- ProjetoComFabio/Database.py
+++ ProjetoComFabio/Database.py
@@ -23,7 +23,30 @@ class Database():
         self.altura = altura
         self.peso = peso
         self.nacionalidade = nacionalidade
-        self.inserirTab = f'INSERT INTO pessoas (nomev, nascimento, sexo, altura, peso, nacionalidade) VALUES ("{self.nomev}", "{self.nascimento}", 
-        "{self.sexo}", "{self.alutra}","{self.peso}", "{self.nacionalidade}")'
+        self.inserirTab = f'INSERT INTO pessoas (nomev, nascimento, sexo, altura, peso, nacionalidade) VALUES ("{self.nomev}", "{self.nascimento}", "{self.sexo}", "{self.altura}","{self.peso}", "{self.nacionalidade}")'
         self.cursor.execute(self.inserirTab)
-        self.cursor.commit()
+
+    def pesquisar(self, ids):
+        self.id = ids
+
+        self.search = f'SELECT id FROM pessoas WHERE id = "{ids}"'
+        self.cursor.execute(self.search)
+        self.resultado = self.cursor.fetchall()
+        print(self.resultado)
+      
+    def deletar(self, ids):
+        self.ids = ids
+        self.delet = f'DELETE FROM pessoas WHERE id = "{ids}"'
+        self.cursor.execute(self.delet)
+
+    def update(self, ids, nomev, nascimento, sexo, altura, peso, nacionalidade):
+        self.ids = ids
+        self.nomev = nomev
+        self.nascimento = nascimento
+        self.sexo = sexo
+        self.altura = altura
+        self.peso = peso
+        self.nacionalidade = nacionalidade
+
+        self.atualizar = f'UPDATE pessoas set nomev = "{self.nomev}", nascimento = "{self.nascimento}", sexo = "{self.sexo}", altura = "{self.altura}", peso = "{self.peso}", nacionalidade = "{self.nacionalidade} "WHERE id = {self.ids}'
+        self.atual = self.cursor.execute(self.atualizar)
--- ProjetoComFabio/main.py
+++ ProjetoComFabio/main.py
@@ -1,5 +1,16 @@
 from Tela import Login
 from Database import Database
 
+ids = '2'
+nomev = "Baby"
+nascimento = "2005-03-14"
+sexo = 'F'
+altura = '1.40'
+peso = '55'
+nacionalidade = "Chines"
 
+sgbd = Database()
+sgbd.conectar()
+sgbd.update(ids, nomev, nascimento, sexo, altura, peso, nacionalidade)
+sgbd.desconectar()
 tela = Login()
