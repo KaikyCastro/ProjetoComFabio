b2cff2a803b701decd880d7bf9776591e4f4c4c7
Kaiky Castro <kaiky.castro7@hotmail.com>
2025-01-06 19:02:32 -0300

    Aparentemente funcionanddo

--- ProjetoComFabio/Database.py
+++ ProjetoComFabio/Database.py
@@ -1,6 +1,81 @@
 import mysql.connector
 lista = []
 
+class Database_colaborador():
+    def __init__(self):
+        pass
+
+    def conectar(self):
+        try:
+            self.conexao = mysql.connector.connect(
+                                              host='localhost',
+                                              user='root',
+                                              password='',
+                                              database='Propriedade_Produtiva')
+            self.cursor = self.conexao.cursor()
+        except mysql.connector.DatabaseError:
+            print("Nao foi possivel conectar ao banco dados")
+
+    def desconectar(self):
+        try:
+            self.conexao.close()
+            self.cursor.close()
+        except mysql.connector.DatabaseError:
+            print("Nao foi possivel desconectar o banco de dados")
+
+    def inserir_colaborador(self, cpf, cargo, nome, telefone, senha):
+        self.cpf = cpf
+        self.cargo = cargo
+        self.nome = nome
+        self.telefone = telefone
+        self.senha = senha
+
+        self.conectar()
+        try:
+            if self.cpf == '' or self.cargo == '' or self.nome == '' or self.telefone == '' or self.senha == '':
+                print("Todos os campos sao obrigatorios")
+            else:
+                self.inserirTabColaborador = f'INSERT INTO Colaborador (cpf, cargo, nome, telefone, senha) VALUES ("{self.cpf}", "{self.cargo}", "{self.nome}", "{self.telefone}", "{self.senha}")'
+                self.cursor.execute(self.inserirTabColaborador)
+                self.desconectar()
+        except ValueError:
+            print("Erro nos valores")
+            self.desconectar()
+
+    def pesquisar_colaborador(self, cpf):
+        self.cpf = cpf
+
+        self.conectar()
+        self.search = f'SELECT cpf FROM Colaborador WHERE cpf = "{cpf}"'
+        self.cursor.execute(self.search)
+        self.resultado = self.cursor.fetchall()
+        print(self.resultado)
+        if (self.resultado == lista):
+            print("Nao existe esse cpf cadastrado!")
+            
+        self.desconectar()
+
+    def deletar_colaborador(self, cpf):
+        self.cpf = cpf
+
+        self.conectar()
+        self.delet = f'DELETE FROM Colaborador WHERE cpf = "{cpf}"'
+        self.cursor.execute(self.delet)
+        self.desconectar()
+
+    def update_colaborador(self, cpf, cargo, nome, telefone, senha):
+        self.cpf = cpf
+        self.cargo = cargo
+        self.nome = nome
+        self.telefone = telefone
+        self.senha = senha
+
+        self.conectar()
+        self.atualizar = f'UPDATE Colaborador SET cargo = "{self.cargo}", nome = "{self.nome}", telefone = "{self.telefone}", senha = "{self.senha}" WHERE cpf = "{self.cpf}"'
+        self.atual = self.cursor.execute(self.atualizar)
+        self.desconectar()
+
+
 
 class Database_cliente():
     def __init__(self):
@@ -24,7 +99,7 @@ class Database_cliente():
         except mysql.connector.DatabaseError:
             print("Nao foi possivel desconectar o banco de dados")
 
-    def inserir_cliente_sem_Incra_CAR(self, cpf, nome, nascimento, sexo, estado_civil, telefone):
+    def inserir_cliente(self, cpf, nome, nascimento, sexo, estado_civil, telefone):
         self.cpf = cpf
         self.nome = nome
         self.nascimento = nascimento
@@ -32,50 +107,39 @@ class Database_cliente():
         self.estado_civil = estado_civil
         self.telefone = telefone
      
+        self.conectar()
         try:
             if self.cpf == '' or self.nome == '' or self.nascimento == '' or self.sexo == '' or self.estado_civil == ''  or self.telefone == '':
                 print("Todos os campos sao obrigatorios")
             else:
                 self.inserirTabCliente = f'INSERT INTO Cliente (cpf, nome, nascimento, sexo, estado_civil, telefone) VALUES ("{self.cpf}", "{self.nome}", "{self.nascimento}", "{self.sexo}", "{self.estado_civil}", "{self.telefone}")'
                 self.cursor.execute(self.inserirTabCliente)
-        except ValueError:
-            print("Erro nos valores")
+                self.desconectar()
 
-    def inserir_cliente(self, cpf, nome, nascimento, sexo, estado_civil, telefone, CAR, INCRA):
-        self.cpf = cpf
-        self.nome = nome
-        self.nascimento = nascimento
-        self.sexo = sexo
-        self.estado_civil = estado_civil
-        self.telefone = telefone
-        self.CAR = CAR
-        self.INCRA = INCRA
-
-        try:
-            if self.cpf == '' or self.nome == '' or self.nascimento == '' or self.sexo == '' or self.estado_civil == ''  or self.telefone == '':
-                print("Todos os campos sao obrigatorios")
-            else:
-                self.inserirTabCliente = f'INSERT INTO Cliente (cpf, nome, nascimento, sexo, estado_civil, telefone, CAR, INCRA) VALUES ("{self.cpf}", "{self.nome}", "{self.nascimento}", "{self.sexo}", "{self.estado_civil}", "{self.telefone}", "{self.CAR}", "{self.INCRA}")'
-                self.cursor.execute(self.inserirTabCliente)
         except ValueError:
             print("Erro nos valores")
 
     def pesquisar_cliente(self, cpf):
         self.cpf = cpf
+
+        self.conectar()
         self.search = f'SELECT cpf FROM Cliente WHERE cpf = "{cpf}"'
         self.cursor.execute(self.search)
         self.resultado = self.cursor.fetchall()
         print(self.resultado)
 
-        
         if (self.resultado == lista):
             print("Nao existe esse cpf cadastrado!")
-        
+            
+        self.desconectar()
       
     def deletar_cliente(self, cpf):
         self.cpf = cpf
+
+        self.conectar()
         self.delet = f'DELETE FROM Cliente WHERE cpf = "{cpf}"'
         self.cursor.execute(self.delet)
+        self.desconectar()
 
     def update_cliente(self, cpf, nome, nascimento, sexo, estado_civil, telefone, CAR, INCRA):
         self.cpf = cpf
@@ -87,5 +151,105 @@ class Database_cliente():
         self.CAR = CAR
         self.INCRA = INCRA
 
+        self.conectar()
         self.atualizar = f'UPDATE Cliente SET nome = "{self.nome}", nascimento = "{self.nascimento}", sexo = "{self.sexo}", estado_civil = "{self.estado_civil}", telefone = "{self.telefone}", CAR = "{self.CAR}", INCRA = "{self.INCRA}" WHERE cpf = "{self.cpf}"'
         self.atual = self.cursor.execute(self.atualizar)
+        self.desconectar()
+
+
+class Database_propriedade():
+    def __init__(self):
+        pass
+
+    def conectar(self):
+        try:
+            self.conexao = mysql.connector.connect(
+                                              host='localhost',
+                                              user='root',
+                                              password='',
+                                              database='Propriedade_Produtiva')
+            self.cursor = self.conexao.cursor()
+        except mysql.connector.DatabaseError:
+            print("Nao foi possivel conectar ao banco dados")
+
+    def desconectar(self):
+        try:
+            self.conexao.close()
+            self.cursor.close()
+        except mysql.connector.DatabaseError:
+            print("Nao foi possivel desconectar o banco de dados")
+
+    def inserir_propriedade(self, endereco, nome_da_propriedade):
+        self.endereco = endereco
+        self.nome_da_propriedade = nome_da_propriedade
+
+        self.conectar()
+        try:
+            if self.endereco == '' or self.nome_da_propriedade == '':
+                print("Todos os campos sao obrigatorios")
+            else:
+                self.inserirTabPropriedade = f'INSERT INTO Propriedade (endereco, nome_da_propriedade) VALUES ("{self.endereco}", "{self.nome_da_propriedade}")'
+                self.cursor.execute(self.inserirTabPropriedade)
+                self.desconectar()
+        except ValueError:
+            print("Erro nos valores")
+            self.desconectar()
+
+    def inserir_propriedade_car_incra(self, endereco, nome_da_proprieddade, CAR, INCRA):
+        self.endereco = endereco
+        self.nome_da_propriedade = nome_da_proprieddade
+        self.CAR = CAR
+        self.INCRA = INCRA
+
+        self.conectar()
+        try:
+            if self.endereco == '' or self.nome_da_propriedade == '' or self.CAR == '' or self.INCRA == '':
+                print("Todos os campos sao obrigatorios")
+            else:
+                self.inserirTabPropriedade = f'INSERT INTO Propriedade (endereco, nome_da_propriedade, CAR, INCRA) VALUES ("{self.endereco}", "{self.nome_da_propriedade}", "{self.CAR}", "{self.INCRA}")'
+                self.cursor.execute(self.inserirTabPropriedade)
+                self.desconectar()
+        except ValueError:
+            print("Erro nos valores")
+            self.desconectar()
+
+    def pesquisar_propriedade(self, endereco):
+        self.endereco = endereco
+
+        self.conectar()
+        self.search = f'SELECT endereco FROM Propriedade WHERE endereco = "{endereco}"'
+        self.cursor.execute(self.search)
+        self.resultado = self.cursor.fetchall()
+        print(self.resultado)
+        if (self.resultado == lista):
+            print("Nao existe esse endereco cadastrado!")
+            
+        self.desconectar()
+
+    def deletar_propriedade(self, endereco):
+        self.endereco = endereco
+
+        self.conectar()
+        self.delet = f'DELETE FROM Propriedade WHERE endereco = "{endereco}"'
+        self.cursor.execute(self.delet)
+        self.desconectar()
+
+    def update_propriedade(self, endereco, nome_da_propriedade):
+        self.endereco = endereco
+        self.nome_da_propriedade = nome_da_propriedade
+
+        self.conectar()
+        self.atualizar = f'UPDATE Propriedade SET nome_da_propriedade = "{self.nome_da_propriedade}" WHERE endereco = "{self.endereco}"'
+        self.atual = self.cursor.execute(self.atualizar)
+        self.desconectar()
+
+    def update_propriedade_car_incra(self, endereco, nome_da_propriedade, CAR, INCRA):
+        self.endereco = endereco
+        self.nome_da_propriedade = nome_da_propriedade
+        self.CAR = CAR
+        self.INCRA = INCRA
+
+        self.conectar()
+        self.atualizar = f'UPDATE Propriedade SET nome_da_propriedade = "{self.nome_da_propriedade}", CAR = "{self.CAR}", INCRA = "{self.INCRA}" WHERE endereco = "{self.endereco}"'
+        self.atual = self.cursor.execute(self.atualizar)
+        self.desconectar()
--- ProjetoComFabio/main.py
+++ ProjetoComFabio/main.py
@@ -1,7 +1,7 @@
 from Tela import Login
-from Database import Database_cliente
+from Database import Database_cliente, Database_colaborador, Database_propriedade
 
-cpf = "134.301.123-09"
+cpf = "134.121.123-09"
 nome = "Lais Mickaelly Pereira da Silva"
 nascimento = "2004-05-30"
 sexo = "F"
@@ -9,14 +9,16 @@ estado_civil = "Solteiro(a)"
 telefone = "83998642050"
 INCRA = "d1ds"
 CAR = "d2s"
+endereco = "Taquara"
+nome_da_propriedade = "Fazenda do Taquara"
 
-sgbd = Database_cliente()
-sgbd.conectar()
-sgbd.inserir_cliente_sem_Incra_CAR(cpf, nome, nascimento, sexo, estado_civil, telefone)
-#sgbd.deletar_cliente(cpf)
-#sgbd.inserir_cliente(cpf, nome, nascimento, sexo, estado_civil, telefone, INCRA, CAR)
-#sgbd.pesquisar_cliente(cpf)
-#sgbd.update_cliente(cpf, nome, nascimento, sexo, estado_civil, telefone, INCRA, CAR)
-sgbd.desconectar()
+'''sgbd_cliente = Database_cliente()
+sgbd_cliente.inserir_cliente_sem_Incra_CAR(cpf, nome, nascimento, sexo, estado_civil, telefone)
+sgbd_cliente.deletar_cliente(cpf)
+sgbd_cliente.inserir_cliente(cpf, nome, nascimento, sexo, estado_civil, telefone, INCRA, CAR)
+sgbd_cliente.pesquisar_cliente(cpf)
+sgbd_cliente.update_cliente(cpf, nome, nascimento, sexo, estado_civil, telefone, INCRA, CAR)'''
 
+prop = Database_propriedade()
+prop.inserir_propriedade(endereco, nome_da_propriedade)
 #tela = Login()
