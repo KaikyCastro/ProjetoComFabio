c01e2f04e7eeee35fd1719325221cfb55211cefb
Kaiky Castro <kaiky.castro7@hotmail.com>
2025-01-06 18:41:08 -0300

    a

--- ProjetoComFabio/main.py
+++ ProjetoComFabio/main.py
@@ -1,7 +1,7 @@
 from Tela import Login
 from Database import Database_cliente
 
-cpf = "134.301.992-09"
+cpf = "134.301.123-09"
 nome = "Lais Mickaelly Pereira da Silva"
 nascimento = "2004-05-30"
 sexo = "F"
@@ -12,11 +12,11 @@ CAR = "d2s"
 
 sgbd = Database_cliente()
 sgbd.conectar()
-#sgbd.inserir_cliente_sem_Incra_CAR(cpf, nome, nascimento, sexo, estado_civil, telefone)
+sgbd.inserir_cliente_sem_Incra_CAR(cpf, nome, nascimento, sexo, estado_civil, telefone)
 #sgbd.deletar_cliente(cpf)
 #sgbd.inserir_cliente(cpf, nome, nascimento, sexo, estado_civil, telefone, INCRA, CAR)
 #sgbd.pesquisar_cliente(cpf)
-sgbd.update_cliente(cpf, nome, nascimento, sexo, estado_civil, telefone, INCRA, CAR)
+#sgbd.update_cliente(cpf, nome, nascimento, sexo, estado_civil, telefone, INCRA, CAR)
 sgbd.desconectar()
 
 #tela = Login()
