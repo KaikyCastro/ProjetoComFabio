834075cbf3ff8b4cfaaf92a93c6fbf27ddc5326a
Kaiky Castro <kaiky.castro7@hotmail.com>
2025-01-07 08:39:19 -0300

    Começando interface gráfica

--- ProjetoComFabio/main.py
+++ ProjetoComFabio/main.py
@@ -1,6 +1,7 @@
 from Database import Database_cliente, Database_colaborador, Database_propriedade
 
-cpf = "124.301.994-09"
+
+'''cpf = "124.301.994-09"
 nome = "Lais Mickaelly Pereira da Silva"
 nascimento = "2004-05-30"
 sexo = "F"
@@ -12,11 +13,11 @@ endereco = "Taquara"
 nome_da_propriedade = "Fazenda do Taquara"
 
 sgbd_cliente = Database_cliente()
-#sgbd_cliente.inserir_cliente_sem_Incra_CAR(cpf, nome, nascimento, sexo, estado_civil, telefone)
-#sgbd_cliente.deletar_cliente(cpf)
+sgbd_cliente.inserir_cliente_sem_Incra_CAR(cpf, nome, nascimento, sexo, estado_civil, telefone)
+sgbd_cliente.deletar_cliente(cpf)
 sgbd_cliente.inserir_cliente(cpf, nome, nascimento, sexo, estado_civil, telefone)
-#sgbd_cliente.pesquisar_cliente(cpf)
-#sgbd_cliente.update_cliente(cpf, nome, nascimento, sexo, estado_civil, telefone, INCRA, CAR)
+sgbd_cliente.pesquisar_cliente(cpf)
+sgbd_cliente.update_cliente(cpf, nome, nascimento, sexo, estado_civil, telefone, INCRA, CAR)
 
-#prop = Database_propriedade()
-#prop.inserir_propriedade(endereco, nome_da_propriedade)
+prop = Database_propriedade()
+prop.inserir_propriedade(endereco, nome_da_propriedade)'''
